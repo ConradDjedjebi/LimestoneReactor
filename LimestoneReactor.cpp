6d63a0458d95efda9339ddc6aeef14c185138e1f
Conrad <conrad.djedjebi@gmail.com>
2018-10-12 19:59:10 +0200

    initial commit

---
+++ LimestoneReactor.cpp
@@ -0,0 +1,27 @@
+/****************************************************************************************************
+  LimestoneReactor.cpp
+
+  author : Conrad Djedjebi
+  version v1.0
+  License: GNU AGPLv3
+
+  A limestone reactor monitoring software for Arduino
+
+  This software is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Affero General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Affero General Public License for more details.
+  You should have received a copy of the GNU Affero General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Version Modified By Date     Comments
+------- ----------- -------- --------
+0001    C DJEDJEBI    xx/xx/xx ..
+****************************************************************************************************/
+
+#include "LIMESTONEREACTOR.h"
---
+++ README.md
@@ -0,0 +1,17 @@
+# Arduino Limestone Reactor
+Copyright (c) 2018 Conrad Djedjebi.
+
+
+## PURPOSE OF THIS C++ PROGRAM:
+
+- Monitor a limestone reactor
+
+
+## RESTRICTIONS AND LIMITATIONS:
+
+- Designed for Arduino ecosystem
+
+
+## COPYRIGHTS:
+
+- This C++ program was written by Conrad Djedjebi and is covered by the AGPLv3
